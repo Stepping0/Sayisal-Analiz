b93982bde8457a2dd46c057750369951e5e7d4be
Stepping <91383276+Stepping0@users.noreply.github.com>
2022-10-17 22:45:26 +0300

    Add files via upload

---
+++ Sayısal.analiz/Sayısal.analiz/Sayısal.analiz.cpp
@@ -0,0 +1,37 @@
+﻿// Sayısal.analiz.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+
+// Pascal üçgeni için matematik işlemlerinin yapıldığı fonksiyon
+void triangle_pascal(int row_num) 
+{
+    // her satırın yazdırılması için döngü
+    for (long n = 1; n <= row_num; n++)
+    {
+        // üçgensel çıktı için döngü
+        for (int r = 1; r < row_num - n + 1; r++)
+            cout << "  ";
+
+        // pascal üçgeninin kombinasyon formullü ile çıktısı
+        int val = 1;
+        for (long r = 1; r <= n; r++) {
+            cout << val << "   ";
+
+            val = val * (n - r) / r;
+        }
+        cout << endl;
+    }
+}
+
+int main() 
+{
+    cout << "\t\t\tPascal\'s triangle\n\n ";
+    cout << "Enter the number of row :" << endl;
+
+    long row_num;
+    cin >> row_num;
+    triangle_pascal(row_num);
+    return 1;
+}
